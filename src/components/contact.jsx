9de1d44275c33b5de1e841548725dd67d95c4496
nechir123 <nechir.salimi@student.oulu.fi>
2021-08-16 17:51:19 +0300

    more changes

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -39,11 +39,7 @@ export const Contact = (props) => {
           <div className='col-md-8'>
             <div className='row'>
               <div className='section-title'>
-                <h2>Get In Touch</h2>
-                <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
-                </p>
+                <h2>Ota yhteyttä</h2>
               </div>
               <form name='sentMessage' validate onSubmit={handleSubmit}>
                 <div className='row'>
